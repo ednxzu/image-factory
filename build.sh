0d16e5bbb758eda0fc1818a6008ea1e4bbfd14ef
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-08-28 23:45:46 +0200

    started to implement build script for automation

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
 
-${BUILD_GROUP:?Please specify a build group}
+##### functions #####
+function get_packer_build_list {
+  raw_list=($(tree "$(dirname $0)/ansible/inventory/production" -L 1 -J --noreport | jq -r .[].contents[].name))
+  echo ${raw_list[@]%.*}
+}
 
-ansible-playbook -i ansible/inventory/production/ansible-runners.yml \
-  ansible/01_generate_templates.yml \
-  -e build_group=ansible_docker_node
+function main {
+  shopt -s extglob
+  build_list="@($(get_packer_build_list | sed -e 's/ /\|/g'))"
+
+  case $BUILD_GROUP in
+    $build_list)
+      echo "#####  Building $BUILD_GROUP  #####"
+      ansible-playbook -i ansible/inventory/production/$BUILD_GROUP.yml \
+      ansible/01_generate_templates.yml \
+      -e build_group=all
+      ;;
+    *)
+      echo "No build_group specified or build_group does not exist. Aborting."
+      ;;
+  esac
+}
+
+##### execution #####
+main "$@"

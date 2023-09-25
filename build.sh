9e5358337037d8ef0254d3ad2e885321d23ba4f1
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-09-25 20:36:34 +0200

    pipeline should work

--- build.sh
+++ build.sh
@@ -1,34 +1,38 @@
 #! /bin/bash
 
 ##### variables #####
-export VAULT_ADDR=https://vault.ednz.fr
-if [[ $TEST -ne 'true' ]]; then
+VAULT_ADDR=https://vault.ednz.fr
+if [ "$TEST" != "true" ]; then
   unset VAULT_TOKEN
-  ${VAULT_ROLE_ID:?VAULT_ROLE_ID needs to be set in order to proceed}
-  ${VAULT_SECRET_ID:?VAULT_SECRET_ID needs to be set in order to proceed}
+  if [ -z "$VAULT_ROLE_ID" ]; then
+    echo "VAULT_ROLE_ID needs to be set in order to proceed"
+    return 1
+  fi
+
+  if [ -z "$VAULT_SECRET_ID" ]; then
+    echo "VAULT_SECRET_ID needs to be set in order to proceed"
+    return 1
+  fi
 fi
 
 ##### functions #####
 function get_packer_build_list {
-  raw_list=($(tree "$(dirname $0)/ansible/inventory/production" -L 1 -J --noreport | jq -r .[].contents[].name))
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  raw_list=($(tree "${SCRIPT_DIR}/ansible/inventory/production" -L 1 -J --noreport | jq -r .[].contents[].name))
   echo ${raw_list[@]%.*}
 }
 
 function vault_login_approle {
-  export VAULT_TOKEN=$(curl --silent --request POST --data "{\"role_id\":\"${VAULT_ROLE_ID}\",\"secret_id\":\"${VAULT_SECRET_ID}\"}" \
-  $VAULT_ADDR/v1/auth/approle/login | jq -r .auth.client_token)
+  curl --silent --request POST --data "{\"role_id\":\"${VAULT_ROLE_ID}\",\"secret_id\":\"${VAULT_SECRET_ID}\"}" $VAULT_ADDR/v1/auth/approle/login | jq -r .auth.client_token
 }
-
-#TODO
-# curl --silent --request POST --data "{\"role_id\":\"${ROLE_ID}\",\"secret_id\":\"${SECRET_ID}\"}" https://vault.ednz.fr/v1/auth/approle/login | jq -r .auth.client_token
 
 function main {
   shopt -s extglob
-  if [[ $TEST -ne 'true' ]]; then
-    vault_login_approle
+  if [ "$TEST" != "true" ]; then
+    export VAULT_ADDR=$VAULT_ADDR
+    export VAULT_TOKEN=$(vault_login_approle)
   fi
   build_list="@($(get_packer_build_list | sed -e 's/ /\|/g'))"
-
   case $BUILD_GROUP in
     $build_list)
       echo "#####  Building $BUILD_GROUP  #####"
@@ -38,7 +42,7 @@ function main {
       ;;
     *)
       echo "No build_group specified or build_group does not exist. Aborting."
-      exit 1
+      return 1
       ;;
   esac
 }
